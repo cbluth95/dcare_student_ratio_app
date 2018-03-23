15587b41d7b7d4adc55791d1ccbd330d6949f741
CODY-PC\Cody <crackerjack0903@gmail.com>
2018-03-22 23:57:14 -0600

    added check for empty rows in the .csv and added more objects

--- test.js
+++ test.js
@@ -2,20 +2,28 @@ function toJson(csvData) {
     var data = [];
     var rows = csvData.split("\n");
     var colNames = rows[0].split(",");
+
+    if(typeof(rows.length) != "") {
+        rows.pop();
+        console.log("Removed an Empty Row")
+    }
     for (var i = 1; i < rows.length; i++) {
         var record = {};
+        
         var sections = rows[i].split(",");
+        
         for (var j = 0; j < sections.length; j++) {
             record[colNames[j]] = sections[j];
 
         }
         data.push(record);
+
     }
     return data;
 
 }
 
-// convert .CSV to JSON and Parse first and last name
+// Convert .CSV to JSON and Parse first and last name
 
     var csvFile = new XMLHttpRequest();
     csvFile.open("GET", "test.csv", true);
@@ -24,17 +32,30 @@ function toJson(csvData) {
             if (csvFile.status === 200 || csvFile.status == 0) {
                 var allText = csvFile.responseText;
                 var result = toJson(allText);
+                
                 var strResult = JSON.stringify(result);
                 var obj = JSON.parse(strResult);
 
-                //Get Name
+                //Get info
                 var text = "";
                 var i;
+                
                 for (i = 0; i < obj.length; i++) {
-                    text += obj[i].first_name + " " + obj[i].last_name + "<br>"; // + " " + obj[i].last_name + "<br>" removed last name
+                    var fName = obj[i].first_name;
+                    var lName = obj[i].last_name;
+                    var bDate = obj[i].birthdate;
+                    var homeRoom = obj[i].room_1;
+                    var pEmail = obj[i].parent_1_email;
+
+        
+
+                    //Set info
+                    text += fName + " " + lName + " " + bDate + " " + homeRoom + " " + pEmail + "<br>"; // + " " + obj[i].last_name + "<br>" removed last name
+                    
+                    
                 }
 
-                //Set Name
+                //Set Data
                 document.getElementById("test").innerHTML = text;
             }
             
