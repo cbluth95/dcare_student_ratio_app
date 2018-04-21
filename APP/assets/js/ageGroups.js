0d41c2cccb5701a668af4a52bfb950d3503a56b5
CODY-PC\Cody <crackerjack0903@gmail.com>
2018-04-21 17:24:13 -0600

    clean up

--- APP/assets/js/ageGroups.js
+++ APP/assets/js/ageGroups.js
@@ -1,3 +1,151 @@
+// Convert .CSV to JSON and Parse first and last name
+
+var dataCSV = new XMLHttpRequest();
+dataCSV.open("GET", "data.csv", true);
+dataCSV.onreadystatechange = function () {
+    if (dataCSV.readyState === 4) {
+        if (dataCSV.status === 200 || dataCSV.status == 0) {
+            var allText = dataCSV.responseText;
+            //Parse JSON Objects
+            var result = toJson(allText);
+            var strResult = JSON.stringify(result);
+            var obj = JSON.parse(strResult);
+
+
+
+            //Get Child Info
+
+            // var fNames = document.getElementById("chldfNames");
+            // var chldAge = document.getElementById("chldAge");
+            // var chldAgeUntil = document.getElementById("chldAgeUntil");
+
+
+            for (i = 0; i < obj.length; i++) {
+
+                var fName = obj[i].first_name;
+                var lName = obj[i].last_name;
+                var bDate = obj[i].birthdate;
+                var age = getAge(bDate);
+                var ageUntil = getAgeUntil(bDate);
+
+
+
+
+                //Infant Data
+                if (age === 0) {
+                    //Get <tbody> Element
+                    var infantData = document.getElementById("infantData");
+
+                    var trEl = document.createElement("tr");
+                    infantData.appendChild(trEl);
+                    //Count set Id for Each Child In tr container
+                    var elCount = document.getElementById("infantData").childElementCount;
+                    var trId = document.createElement("th");
+                    trId.innerHTML = elCount;
+                    trEl.appendChild(trId);
+
+                    //Infant Data   
+                    var trName = document.createElement("th");
+                    trName.innerHTML = fName + ' ' + lName;
+                    trEl.appendChild(trName);
+
+                    var trAge = document.createElement("th");
+                    trAge.innerHTML = age + ' Years Old';
+                    trEl.appendChild(trAge);
+
+                    var trAgeUntil = document.createElement("th");
+                    trAgeUntil.innerHTML = ageUntil;
+                    trEl.appendChild(trAgeUntil);
+
+
+                }
+                //Wobblers 
+                else if (age > 0 && age < 2) {
+
+                    var wobblerData = document.getElementById("wobblerData");
+
+                    var trEl = document.createElement("tr");
+                    wobblerData.appendChild(trEl);
+
+
+                    //Count set Id for Each Child In tr container
+                    var elCount = document.getElementById("wobblerData").childElementCount;
+                    var trId = document.createElement("th");
+                    trId.innerHTML = elCount;
+                    trEl.appendChild(trId);
+                    //Wobbler Data
+                    var trName = document.createElement("th");
+                    trName.innerHTML = fName + ' ' + lName;
+                    trEl.appendChild(trName);
+
+                    var trAge = document.createElement("th");
+                    trAge.innerHTML = age + ' Years Old';
+                    trEl.appendChild(trAge);
+
+                    var trAgeUntil = document.createElement("th");
+                    trAgeUntil.innerHTML = ageUntil;
+                    trEl.appendChild(trAgeUntil);
+                }
+                //Toddlers
+                else if (age > 1 && age < 3) {
+
+                    var toddlerData = document.getElementById("toddlerData");
+                    //Create container
+                    var trEl = document.createElement("tr");
+                    toddlerData.appendChild(trEl);
+
+
+                    //Count set Id for Each Child In tr container
+                    var elCount = document.getElementById("toddlerData").childElementCount;
+                    var trId = document.createElement("th");
+                    trId.innerHTML = elCount;
+                    trEl.appendChild(trId);
+                    //Toddler Data
+                    var trName = document.createElement("th");
+                    trName.innerHTML = fName + ' ' + lName;
+                    trEl.appendChild(trName);
+
+                    var trAge = document.createElement("th");
+                    trAge.innerHTML = age + ' Years Old';
+                    trEl.appendChild(trAge);
+
+                    var trAgeUntil = document.createElement("th");
+                    trAgeUntil.innerHTML = ageUntil;
+                    trEl.appendChild(trAgeUntil);
+                }
+                //Toddlers
+                else if (age > 2 || age == 'NaN') {
+
+                    var toddlerData = document.getElementById("schoolAgeData");
+                    //Create container
+                    var trEl = document.createElement("tr");
+                    toddlerData.appendChild(trEl);
+
+
+                    //Count set Id for Each Child In tr container
+                    var elCount = document.getElementById("schoolAgeData").childElementCount;
+                    var trId = document.createElement("th");
+                    trId.innerHTML = elCount;
+                    trEl.appendChild(trId);
+                    //Toddler Data
+                    var trName = document.createElement("th");
+                    trName.innerHTML = fName + ' ' + lName;
+                    trEl.appendChild(trName);
+
+                    var trAge = document.createElement("th");
+                    trAge.innerHTML = age + ' Years Old';
+                    trEl.appendChild(trAge);
+
+
+                    var trAgeUntil = document.createElement("th");
+                    trAgeUntil.innerHTML = ageUntil;
+                    trEl.appendChild(trAgeUntil);
+                }
+            }
+        }
+    }
+}
+
 function toJson(csvData) {
     var data = [];
     var rows = csvData.split("\n");
@@ -20,133 +168,6 @@ function toJson(csvData) {
 
     }
     return data;
-}
-
-// Convert .CSV to JSON and Parse first and last name
-
-var dataCSV = new XMLHttpRequest();
-dataCSV.open("GET", "data.csv", true);
-dataCSV.onreadystatechange = function () {
-    if (dataCSV.readyState === 4) {
-        if (dataCSV.status === 200 || dataCSV.status == 0) {
-            var allText = dataCSV.responseText;
-            //Parse JSON Objects
-            var result = toJson(allText);
-            var strResult = JSON.stringify(result);
-            var obj = JSON.parse(strResult);
-
-
-            //Get Child Info
-
-            // var fNames = document.getElementById("chldfNames");
-            // var chldAge = document.getElementById("chldAge");
-            // var chldAgeUntil = document.getElementById("chldAgeUntil");
-
-
-            for (i = 0; i < obj.length; i++) {
-                var id = i;
-                
-                var fName = obj[i].first_name;
-                var lName = obj[i].last_name;
-                var bDate = obj[i].birthdate;
-                var homeRoom = obj[i].room_1;
-                var pEmail = obj[i].parent_1_email;
-                var age = getAge(bDate);
-                var ageUntil = getAgeUntil(bDate);
-
-
-                //Create Elements
-                // var fullName = document.createElement("p");
-                // var fNameEl = document.createElement("p");
-                // var cAge = document.createElement("p");
-                // var cAgeUntil = document.createElement("p");
-
-                //Set Elements
-                // fullName.innerHTML = fName + ' ' + lName;
-                // fNameEl.innerHTML = fName;
-                // cAge.innerHTML = age + 'y';
-                // cAgeUntil.innerHTML = ageUntil;
-
-
-                //Infant Data
-                if (age === 0) {
-                    console.log(id)
-
-                    var infantData = document.getElementById("infantData");
-                    //Create container
-                    var trEl = document.createElement("tr");
-                    infantData.appendChild(trEl);
-                    //Infant Data
-                    var trId = document.createElement("th");
-                    trId.innerHTML = '#soon';
-                    trEl.appendChild(trId);
-
-
-                    var trName = document.createElement("th");
-                    trName.innerHTML = fName + ' ' + lName;
-                    trEl.appendChild(trName);
-
-                    var trAge = document.createElement("th");
-                    trAge.innerHTML = age + ' Years Old';
-                    trEl.appendChild(trAge);
-
-                    var trAgeUntil = document.createElement("th");
-                    trAgeUntil.innerHTML = ageUntil;
-                    trEl.appendChild(trAgeUntil);
-
-                    //Wobblers
-                } else if (age > 0 && age < 2) {
-
-                    var wobblerData = document.getElementById("wobblerData");
-                    //Create container
-                    var trEl = document.createElement("tr");
-                    wobblerData.appendChild(trEl);
-
-                    //Toddler Data
-                    var trId = document.createElement("th");
-                    trId.innerHTML = '#soon';
-                    trEl.appendChild(trId);
-
-                    var trName = document.createElement("th");
-                    trName.innerHTML = fName + ' ' + lName;
-                    trEl.appendChild(trName);
-
-                    var trAge = document.createElement("th");
-                    trAge.innerHTML = age + ' Years Old';
-                    trEl.appendChild(trAge);
-
-                    var trAgeUntil = document.createElement("th");
-                    trAgeUntil.innerHTML = ageUntil;
-                    trEl.appendChild(trAgeUntil);
-                }
-                //Toddlers
-                else if (age > 1 && age < 3) {
-
-                    var toddlerData = document.getElementById("toddlerData");
-                    //Create container
-                    var trEl = document.createElement("tr");
-                    toddlerData.appendChild(trEl);
-
-                    //Toddler Data
-                    var trId = document.createElement("th");
-                    trId.innerHTML = '#soon';
-                    trEl.appendChild(trId);
-
-                    var trName = document.createElement("th");
-                    trName.innerHTML = fName + ' ' + lName;
-                    trEl.appendChild(trName);
-
-                    var trAge = document.createElement("th");
-                    trAge.innerHTML = age + ' Years Old';
-                    trEl.appendChild(trAge);
-
-                    var trAgeUntil = document.createElement("th");
-                    trAgeUntil.innerHTML = ageUntil;
-                    trEl.appendChild(trAgeUntil);
-                }
-            }
-        }
-    }
 }
 
 function getAge(bDate) {
